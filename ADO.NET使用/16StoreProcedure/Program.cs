0d3476cc67c671f58c67e5070b8c1d9433ba52be
shanzm <2351144690@qq.com>
2020-05-03 00:17:25 +0800

    2020年5月3日 00:17:12- Create：使用存储过程

---
+++ ADO.NET使用/16StoreProcedure/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16StoreProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //转出账户的Id
+            int from = 1;
+            //转入账户的Id
+            int to = 2;
+            //转账金额
+            decimal balance = 10;
+
+            SqlParameter[] param =
+            {
+                //new SqlParameter ("@from",System.Data.SqlDbType.BigInt) {Value=2 },
+                //new SqlParameter("@to",System.Data.SqlDbType.BigInt) {Value=1 },
+                //new SqlParameter ("@balance",System.Data.SqlDbType.Decimal) {Value=10 },
+
+                new SqlParameter ("@from",from),
+                new SqlParameter("@to",to),
+                new SqlParameter ("@balance",balance),
+                //设置为输出参数
+                new SqlParameter ("@returnNum",System.Data.SqlDbType.Int) {Direction=System.Data.ParameterDirection.Output }
+            };
+
+            SqlHelper.ExecuteNonquery("pro_transfer_szmbank", System.Data.CommandType.StoredProcedure, param);
+
+            //根据输出参数判断
+            int outPutparam = Convert.ToInt16(param[3].Value);
+            switch (outPutparam)
+            {
+                case 1: Console.WriteLine($"success:从Id:{from}转账{balance}元到Id：{to}"); break;
+                case 2: Console.WriteLine("error"); break;
+                case 3: Console.WriteLine("余额不足"); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ADO.NET使用/16StoreProcedure/SqlHelper.cs
@@ -0,0 +1,108 @@
+﻿#region
+// ===============================================================================
+// Project Name        :    ThreeLayersWeb.DAL
+// Project Description : 
+// ===============================================================================
+// Class Name          :    SqlHelper
+// Class Version       :    v1.0.0.0
+// Class Description   : 
+// CLR                 :    4.0.30319.18408  
+// Author              :    单志铭(shanzm)
+// Create Time         :    2018-8-14 18:22:59
+// Update Time         :    2018-8-14 18:22:59
+// ===============================================================================
+// Copyright © SHANZM-PC  2018 . All rights reserved.
+// ===============================================================================
+#endregion
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _16StoreProcedure
+{
+    public class SqlHelper
+    {
+        private static readonly string connStr =
+            ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
+        /// <summary>
+        /// 返回查询结果的的表
+        /// </summary>
+        /// <param name="sql">SQL语句或存储过程</param>
+        /// <param name="type">执行类型</param>
+        /// <param name="param">参数</param>
+        /// <returns></returns>
+        public static DataTable GetDataTable(string sql, CommandType type, params  SqlParameter[] param)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(sql, conn))
+                {
+                    if (param != null)
+                    {
+                        adapter.SelectCommand.Parameters.AddRange(param);
+                    }
+
+                    adapter.SelectCommand.CommandType = type;
+                    DataTable da = new DataTable();
+                    adapter.Fill(da);
+                    return da;
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// 返回影响行数
+        /// </summary>
+        /// <param name="sql">SQL语句或存储过程</param>
+        /// <param name="type">执行类型</param>
+        /// <param name="param">参数</param>
+        /// <returns></returns>
+        public static int ExecuteNonquery(string sql, CommandType type, params SqlParameter[] param)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    if (param != null)
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+                    cmd.CommandType = type;
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 返回查询结果的第一行第一个单元格的数据
+        /// </summary>
+        /// <param name="sql">SQL语句或存储过程</param>
+        /// <param name="type">执行类型</param>
+        /// <param name="param">参数</param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string sql, CommandType type, params SqlParameter[] param)
+        {
+            using (SqlConnection conn=new SqlConnection (connStr ))
+            {
+                using (SqlCommand cmd=new SqlCommand (sql,conn))
+                {
+                    if (param !=null )
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+                    cmd.CommandType = type ;
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+    }
+}
