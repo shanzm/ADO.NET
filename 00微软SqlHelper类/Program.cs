b8b60d3e5763d8310056453d7856c235d6d6f997
shanzm_aimu <2351144690@qq.com>
2021-06-12 14:14:50 +0800

    2021年6月12日 14:14:29-Add：微软SQLHelper

---
+++ 00微软SqlHelper类/Program.cs
@@ -0,0 +1,27 @@
+﻿using MyHelper;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _00微软SqlHelper类
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+
+            DataTable result1 = SqlHelper.ExecuteDataset(connStr, CommandType.Text, "select * from Person").Tables[0];
+            //SqlParameter[] sqlParameters =
+            //{
+            //    new SqlParameter ("@Id",2),
+            //    new SqlParameter ("@Age",6)
+            //};
+            //DataTable result2 = SqlHelper.ExecuteDataset(connStr, CommandType.StoredProcedure, "dbo.pro_GetPersonByIdAndAge",sqlParameters).Tables[0];
+            DataTable result3 = SqlHelper.ExecuteDataset(connStr, CommandType.StoredProcedure, "sp_tables").Tables[0];
+            DataTable result4 = SqlHelper .ExecuteDataset(connStr, CommandType.StoredProcedure, "pro_GetPerson").Tables[0];
+            Console.ReadKey();
+        }
+    }
+}
