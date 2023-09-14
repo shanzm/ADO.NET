ea44cb7c061e869258c578d92ead65e7d0c58fc7
shanzm_aimer <2351144690@qq.com>
2023-09-14 17:25:09 +0800

    2023年9月14日 17:24:53-Update：关于DataView的RowFilter

--- 10DataSet和DataTable/Program.cs
+++ 10DataSet和DataTable/Program.cs
@@ -6,9 +6,9 @@ using System.Data;
 
 namespace _10DataSet和DataTable
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //新建一个数据集，名字就叫ds
             DataSet ds = new DataSet("ds");
@@ -32,9 +32,9 @@ namespace _10DataSet和DataTable
             //给表按行添加数据
             dt1.Rows.Add(1, "张三", 24);
             dt1.Rows.Add(2, "李四", 24);
-            dt1.Rows.Add(3, "王五", 24); 
-            #endregion
+            dt1.Rows.Add(3, "王五", 24);
 
+            #endregion
 
             #region 表dt2
 
@@ -56,18 +56,85 @@ namespace _10DataSet和DataTable
             dt2.Rows.Add(1, "张三2", 23);
             dt2.Rows.Add(2, "李四2", 24);
             dt2.Rows.Add(3, "王五2", 25);
+
             #endregion
 
-
             //打印数据集，先循环数据集的每个表再循环每个表的行
-            foreach (DataTable  tb in ds.Tables  )
+            foreach (DataTable tb in ds.Tables)
             {
-                foreach (DataRow  dr in tb.Rows  )
+                foreach (DataRow dr in tb.Rows)
                 {
-                    Console.WriteLine("{0}--{1}--{2}",dr[0],dr[1],dr[2]);
-                    
+                    Console.WriteLine("{0}--{1}--{2}", dr[0], dr[1], dr[2]);
                 }
             }
+
+            #region 案例：用于熟悉DataTable中的各个对象
+
+            //先来建立ds数据库
+            DataSet myds = new DataSet("ds");
+            //再来建立tbClass和tbStudent两个数据表
+            DataTable tbClass = new DataTable("tbClass");
+            DataTable tbStudent = new DataTable("tbStudent");
+            //把两个数据表tbClass和tbStudent加入数据库
+            ds.Tables.Add(tbClass);
+            ds.Tables.Add(tbStudent);
+
+            //建立tbClass两列
+            DataColumn ClassID = new DataColumn("ClassID", typeof(System.String));
+            DataColumn ClassName = new DataColumn("ClassName", typeof(System.String));
+            //设定ClassID列不允许为空
+            ClassID.AllowDBNull = false;
+            //把列加入tbClass表
+            tbClass.Columns.Add(ClassID);
+            tbClass.Columns.Add(ClassName);
+            //设定tdClass表的主键
+            tbClass.PrimaryKey = new DataColumn[] { ClassID };
+
+            //建立tbStudent的三列
+            DataColumn StudentID = new DataColumn("StudentID", typeof(System.String));
+            DataColumn StudentName = new DataColumn("StudentName", typeof(System.String));
+            DataColumn StudentClassID = new DataColumn("StudentClassID", typeof(System.String));
+            //设定StudentID列不允许为空
+            StudentID.AllowDBNull = false;
+            //把列加入tbStudent表
+            tbStudent.Columns.Add(StudentID);
+            tbStudent.Columns.Add(StudentName);
+            tbStudent.Columns.Add(StudentClassID);
+            //设定tbStudent表的主键
+            tbStudent.PrimaryKey = new DataColumn[] { StudentID };
+
+            // 为两个表各加入5条记录
+            for (int i = 1; i <= 5; i++)
+            {
+                //实例化tbClass表的行
+                DataRow tbClassRow = tbClass.NewRow();
+                //为行中每一列赋值
+                tbClassRow["ClassID"] = Guid.NewGuid();
+                tbClassRow["ClassName"] = string.Format("班级{0}", i);
+                //把行加入tbClass表
+                tbClass.Rows.Add(tbClassRow);
+                //实例化tbStudent表的行
+                DataRow tbStudentRow = tbStudent.NewRow();
+                //为行中每一列赋值
+                tbStudentRow["StudentID"] = Guid.NewGuid();
+                tbStudentRow["StudentName"] = string.Format("学生{0}", i);
+                tbStudentRow["StudentclassID"] = tbClassRow["ClassID"];
+                //把行加入tbStudent表
+                tbStudent.Rows.Add(tbStudentRow);
+            }
+
+            foreach (DataRow row in tbClass.Rows)
+            {
+                Console.WriteLine("{0}-----{1}", row[0], row[1]);
+            }
+
+            foreach (DataRow row in tbStudent.Rows)
+            {
+                Console.WriteLine("{0}-----{1}-----{2}", row[0], row[1], row[2]);
+            }
+
+            #endregion
+
             Console.ReadKey();
         }
     }
---
+++ 10_1DataView的RowFilter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+
+namespace _10_1DataView的RowFilter
+{
+    internal class Program
+    {
+        //参考：DataView RowFilter 的语法：https://blog.csdn.net/lingxyd_0/article/details/44490351
+        private static void Main(string[] args)
+        {
+            DataView dataView = GetDataTable().DefaultView;
+
+            dataView.RowFilter = "Id=5";
+            PrintDataTable(dataView.ToTable());
+            PrintDataTable(dataView.Table);
+            //注意区分：ToTable()和Table
+            //DataView和DataTable的区别、联系、以及相互转化方法:https://blog.csdn.net/sihsd/article/details/104096324
+            Console.ReadKey();
+        }
+
+        //构建测试数据源
+
+        private static DataTable GetDataTable()
+        {
+            DataTable dt = new DataTable("Student");
+            DataColumn dcId = new DataColumn("Id", typeof(int));
+            DataColumn dcName = new DataColumn("Name", typeof(string));
+            DataColumn dcAge = new DataColumn("Age", typeof(int));
+            DataColumn dcGender = new DataColumn("Gender", typeof(bool));
+
+            DataColumn[] dataColumns = new DataColumn[] { dcId, dcName, dcAge, dcGender };
+            dt.Columns.AddRange(dataColumns);
+
+            for (int i = 0; i < 20; i++)
+            {
+                DataRow row = dt.NewRow();
+                row["Id"] = i;
+                row["Name"] = "学生" + i;
+                row["Age"] = 10 + i;
+                row["Gender"] = i % 2 == 0;
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
+        private static void PrintDataTable(DataTable dt)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine($"Id:{row[0]}---Name:{row[1]}---Age:{row[2]}----Gender:{row[3]}");
+            }
+        }
+    }
+}
