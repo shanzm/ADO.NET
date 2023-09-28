9047270765be33b4134faea37f217c9d894a00c5
shanzm_aimer <2351144690@qq.com>
2023-09-28 16:59:08 +0800

    2023年9月28日 16:59:05-Update

---
+++ 10_1DataView的RowFilter/PrintDateTable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace _10_1DataView的RowFilter
+{
+    public static class Printer
+    {
+        /// <summary>
+        /// 控制台输出DataTable
+        /// </summary>
+        /// <param name="dt">目标DataTable</param>
+        /// <param name="title">DataTable的标题</param>
+        public static void PrintDataTable(DataTable dt, string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{title}");
+            //拼接列头
+            string colName = "";
+            foreach (DataColumn column in dt.Columns)
+            {
+                colName += "| " + PadRightEx(column.ColumnName, 20);
+            }
+            Console.WriteLine(colName);
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+
+            //循环打印每一行
+            foreach (DataRow row in dt.Rows)
+            {
+                //拼接行数据
+                string strRow = "";
+                foreach (object item in row.ItemArray)
+                {
+                    strRow += "| " + PadRightEx(item.ToString(), 20);
+                }
+                Console.WriteLine(strRow);
+            }
+            Console.WriteLine("");
+        }
+
+        /// <summary>
+        /// 用空格将字符串拼接到指定的长度
+        /// 参考：https://www.cnblogs.com/chenjiahong/articles/2705437.html
+        /// 中文和英文字符占位宽度不一样，进行调整
+        /// </summary>
+        /// <param name="str">目标字符串</param>
+        /// <param name="totalByteCount">拼接后的字符串长度</param>
+        /// <returns></returns>
+        public static string PadRightEx(string str, int totalByteCount)
+        {
+            Encoding coding = Encoding.GetEncoding("gb2312");
+            int dcount = 0;
+            foreach (char ch in str.ToCharArray())
+            {
+                if (coding.GetByteCount(ch.ToString()) == 2)
+                {
+                    dcount++;
+                }
+            }
+            string w = str.PadRight(totalByteCount - dcount);
+            return w;
+        }
+    }
+}
--- 10_1DataView的RowFilter/Program.cs
+++ 10_1DataView的RowFilter/Program.cs
@@ -12,7 +12,10 @@ namespace _10_1DataView的RowFilter
         private static void Main(string[] args)
         {
             //TestToTable();
-            TestColumnFilter();
+            //TestDataView();
+            //TestColumnFilter();
+
+            Test.DemonstrateRowVersion();
 
             Console.ReadKey();
         }
@@ -23,24 +26,48 @@ namespace _10_1DataView的RowFilter
         {
             DataTable dt = GetDataTable();
             DataView dataView = dt.DefaultView;
+
             dataView.RowFilter = "Id=6";
             //DataView的Table属性获取的该DataView原始数据源
-            PrintDataTable(dataView.Table, "dataView.Table");
+            Printer.PrintDataTable(dataView.Table, "dataView.Table");
 
             //DataView的ToTable方法获取的是DataView经过了RowFilter的数据（即：根据现有的DataView中的数据创建一个新的DataTable）
-            PrintDataTable(dataView.ToTable(), "dataView.ToTable()");
+            Printer.PrintDataTable(dataView.ToTable(), "dataView.ToTable()");
 
             dataView.RowFilter = "";
             //注意：★dataView.ToTable()是根据最近一次的rowFilter对原始数据筛选的结果创建DataTable
             //这里我需要对原始数据去重并创建一个新的Datatable，所以我将rowFilter=""置空
             //DataView的ToTable方法可以去重创建DataTable
-            PrintDataTable(dataView.ToTable(true), "对当前DataView去重并创建一个DataTable");
+            Printer.PrintDataTable(dataView.ToTable(true), "对当前DataView去重并创建一个DataTable");
 
             //DataView的ToTable方法可以选择指定的字段创建DataTbale
-            PrintDataTable(dataView.ToTable(false, new string[] { "Name", "Age" }), "选择指定字段创建一个DataTable");
+            Printer.PrintDataTable(dataView.ToTable(false, new string[] { "Name", "Age" }), "选择指定字段创建一个DataTable");
         }
 
-        //2.关于对指定列进行筛选：筛选表达式的基本语法和SQL语法相似
+        //2.关于DateView的一些属性和方法
+        private static void TestDataView()
+        {
+            DataTable dt = GetDataTable();
+            DataView dataView = dt.DefaultView;
+
+            DataRowView dataRowView = dataView.AddNew();
+            dataRowView["Id"] = 9;
+            dataRowView["Name"] = "小明";
+            dataRowView.EndEdit();
+
+            Printer.PrintDataTable(dataView.ToTable(), "新添加一行");
+
+            dataView.Delete(0);
+            Printer.PrintDataTable(dataView.ToTable(), "按照索引，删除第一行");
+
+            dataView.Sort = "CreateTime ASC";
+            Printer.PrintDataTable(dataView.ToTable(), "按照CreateTime字段顺序排序");
+
+            dataView.ApplyDefaultSort = true;
+            Printer.PrintDataTable(dataView.ToTable(), "默认按照主键列 升序排列");
+        }
+
+        //3.关于对指定列进行筛选：筛选表达式的基本语法和SQL语法相似
         private static void TestColumnFilter()
         {
             DataTable dt = GetDataTable();
@@ -48,34 +75,34 @@ namespace _10_1DataView的RowFilter
 
             //1.列名中有特殊字符则可以将列名使用[]包裹
             dataView.RowFilter = "Id=4 AND [Name]='赵六'";
-            PrintDataTable(dataView.ToTable(), "Id=4 AND [Name]='赵六'");
+            Printer.PrintDataTable(dataView.ToTable(), "Id=4 AND [Name]='赵六'");
 
             //2.列值是字符串类型，则值需要使用单引号包裹
             //列值中包含单引号，则需要替换为两个单引号''
             dataView.RowFilter = $"Name='{"''张三''"}'";
             dataView.RowFilter = $"Name='{"'张三'".Replace("'", "''")}'";
-            PrintDataTable(dataView.ToTable(), $"Name='{"''张三''"}'");
+            Printer.PrintDataTable(dataView.ToTable(), $"Name='{"''张三''"}'");
 
             //3.列值是时间，则使用##或''
             dataView.RowFilter = $"CreateTime=#{"2023-8-27 20:10:00"}#";
             dataView.RowFilter = $"CreateTime='2023-8-27 20:10:00'";
-            PrintDataTable(dataView.ToTable(), $"CreateTime=#{"2023-8-27 20:10:00"}#");
+            Printer.PrintDataTable(dataView.ToTable(), $"CreateTime=#{"2023-8-27 20:10:00"}#");
 
             //4.运行符In
             dataView.RowFilter = $"Id IN (1,2) OR CreateTime IN ('2023-7-27 20:10:00')";
-            PrintDataTable(dataView.ToTable(), $"Id IN (1,2) OR CreateTime IN ('2023-7-27 20:10:00')");
+            Printer.PrintDataTable(dataView.ToTable(), $"Id IN (1,2) OR CreateTime IN ('2023-7-27 20:10:00')");
 
             //5.运行符Like
             dataView.RowFilter = $"Name like '%钱%' OR Name like '孙*'";
-            PrintDataTable(dataView.ToTable(), $"Name like '%钱%' OR Name like '孙*'");
+            Printer.PrintDataTable(dataView.ToTable(), $"Name like '%钱%' OR Name like '孙*'");
 
             //6.Boolean运行符
             dataView.RowFilter = $"Age>=19 And Name <> '钱七' And Id Not In (6)";
-            PrintDataTable(dataView.ToTable(), $"Age>=19 And Name <> '钱七' And Id Not In (6)");
+            Printer.PrintDataTable(dataView.ToTable(), $"Age>=19 And Name <> '钱七' And Id Not In (6)");
 
             //7.算术运算符
             dataView.RowFilter = $"Age+10>30";
-            PrintDataTable(dataView.ToTable(), $"Age+10>30");
+            Printer.PrintDataTable(dataView.ToTable(), $"Age+10>30");
 
             //8.
         }
@@ -92,72 +119,18 @@ namespace _10_1DataView的RowFilter
             DataColumn[] dataColumns = new DataColumn[] { dcId, dcName, dcAge, dcGender, dcCreateTime };
             dt.Columns.AddRange(dataColumns);
 
+            //设置主键列
+            dt.PrimaryKey = new DataColumn[] { dcId };
+
             dt.Rows.Add(1, "'张三'", 17, false, "2023-9-27 20:10:00");
             dt.Rows.Add(2, "李四", 18, true, "2023-8-27 20:10:00");
             dt.Rows.Add(3, "王五", 19, false, "2023-7-27 20:10:00");
             dt.Rows.Add(4, "赵六", 19, true, "2023-6-27 20:10:00");
             dt.Rows.Add(5, "钱七", 20, false, "2023-5-27 20:10:00");
             dt.Rows.Add(6, "孙八", 21, false, "2023-4-27 20:10:00");
-            dt.Rows.Add(6, "孙八", 21, false, "2023-4-27 20:10:00");
+            dt.Rows.Add(7, "孙八", 21, false, "2023-4-27 20:10:00");
 
             return dt;
         }
-
-        #region 打印DataTable辅助函数
-
-        /// <summary>
-        /// 控制台输出DataTable
-        /// </summary>
-        /// <param name="dt">目标DataTable</param>
-        /// <param name="title">DataTable的标题</param>
-        private static void PrintDataTable(DataTable dt, string title)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{title}");
-            //拼接列头
-            string colName = "";
-            foreach (DataColumn column in dt.Columns)
-            {
-                colName += "|" + PadRightEx(column.ColumnName, 20);
-            }
-            Console.WriteLine(colName);
-            Console.WriteLine("--------------------------------------------------------------------------------------------------------");
-
-            //循环打印每一行
-            foreach (DataRow row in dt.Rows)
-            {
-                //拼接行数据
-                string strRow = "";
-                foreach (var item in row.ItemArray)
-                {
-                    strRow += "|" + PadRightEx(item.ToString(), 20);
-                }
-                Console.WriteLine(strRow);
-            }
-            Console.WriteLine("");
-        }
-
-        /// <summary>
-        /// 用空格将字符串拼接到指定的长度
-        /// 参考：https://www.cnblogs.com/chenjiahong/articles/2705437.html
-        /// 中文和英文字符占位宽度不一样，进行调整
-        /// </summary>
-        /// <param name="str">目标字符串</param>
-        /// <param name="totalByteCount">拼接后的字符串长度</param>
-        /// <returns></returns>
-        private static string PadRightEx(string str, int totalByteCount)
-        {
-            Encoding coding = Encoding.GetEncoding("gb2312");
-            int dcount = 0;
-            foreach (char ch in str.ToCharArray())
-            {
-                if (coding.GetByteCount(ch.ToString()) == 2)
-                    dcount++;
-            }
-            string w = str.PadRight(totalByteCount - dcount);
-            return w;
-        }
-
-        #endregion
     }
 }
